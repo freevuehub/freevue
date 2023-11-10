8b01cad3be2adf774e5167b115aae314958dbb47
freevuehub <hackish113@gamil.com>
2023-11-10 17:35:56 +0900

    기본 설정 및 폰트 적용

---
+++ constant.ts
@@ -0,0 +1,4 @@
+export const SITE_CONFIG = {
+  TITLE: '프리뷰 블로그' as string,
+  DESCRIPTION: '' as string,
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,34 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      taebaek: ['TAEBAEKfont'],
+      pretendard: [
+        'Pretendard Variable',
+        'Pretendard',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'system-ui',
+        'Roboto',
+        'Helvetica Neue',
+        'Segoe UI',
+        'Apple SD Gothic Neo',
+        'Noto Sans KR',
+        'Malgun Gothic',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'sans-serif',
+      ],
     },
+    extend: {},
   },
   plugins: [],
 }
