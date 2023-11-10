deff9074a4112b1e341ed0fcd3c33619a86fa216
freevuehub <hackish113@gmail.com>
2023-11-10 17:48:03 +0900

    레이아웃 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+'use server'
+
+import Link from 'next/link'
 import { Card } from '~/components'
+import { SITE_CONFIG } from '~/constant'
 
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-taebaek text-[30px]">Hello</h1>
+    <main className="max-w-[1200px] mx-auto">
+      <header>
+        <Link href={{ pathname: '/' }} className="dark:text-white font-taebaek text-[20px]">
+          {SITE_CONFIG.TITLE}
+        </Link>
+      </header>
+      <h1 className="font-taebaek text-[30px]"></h1>
       <Card>
-        <p className="dark:text-white">Hello</p>
+        <p className="dark:text-white">배포 실수로 블로그 날라감..</p>
       </Card>
     </main>
   )
