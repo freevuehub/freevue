418528f427ad426ead925eb2c485befe15498127
freevuehub <hackish113@gmail.com>
2023-11-13 20:31:18 +0900

    컴포넌트 클래스 추가 가능하게 개선

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -5,7 +5,7 @@ import type { IChild } from '~/types'
 type IProps = {} & IChild
 
 const Card: React.FC<IProps> = (props) => {
-  return <div>{props.children}</div>
+  return <div className={props.className || ''}>{props.children}</div>
 }
 
 export default Card
