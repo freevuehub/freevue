1c544a43db7999b180195ab5cecfbabb0a6803d7
freevuehub <hackish113@gmail.com>
2023-11-13 14:03:47 +0900

    페이지 빌드 버그 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,16 @@ import { SITE_CONFIG } from '~/constant'
 import './globals.css'
 
 import type { Metadata } from 'next'
-import type { IChild } from '~/types'
 
 export const metadata: Metadata = {
   title: SITE_CONFIG.TITLE,
   description: SITE_CONFIG.DESCRIPTION,
 }
 
-const RootLayout = (props: IChild) => {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ko" className="dark">
-      <body>{props.children}</body>
+      <body>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 'use server'
 
 import Link from 'next/link'
+import { pipe, range, map, toArray } from '@fxts/core'
+import { Client } from '@notionhq/client'
 import { Card } from '~/components'
 import { SITE_CONFIG } from '~/constant'
 
+const notion = new Client({
+  auth: 'secret_XCVvCFdlfSZOyLPvs8xb7qCABBCHy1qieCs8eA8u5eQ',
+})
+
 const Home = () => {
+  ;(async () => {
+    const listUsersResponse = await notion.databases.retrieve({
+      database_id: 'aaffdb0decad4da5ac6c15456dfe22f7',
+    })
+    console.log(listUsersResponse)
+  })()
+
+  // https://www.notion.so/freevue/aaffdb0decad4da5ac6c15456dfe22f7?v=b99028e495fb401ea973b8d2973b41a3&pvs=4
+
   return (
     <main className="max-w-[1200px] mx-auto">
       <header>
@@ -16,6 +31,16 @@ const Home = () => {
       <Card>
         <p className="dark:text-white">배포 실수로 블로그 날라감..</p>
       </Card>
+      <div className="fixed left-0 right-0 top-0 bottom-0 bg-[red] opacity-5">
+        <div className="w-[1200px] mx-auto h-full bg-green-950 flex gap-[30px]">
+          {pipe(
+            12,
+            range,
+            map((index) => <div key={index} className="bg-amber-300 flex-1 h-full"></div>),
+            toArray
+          )}
+        </div>
+      </div>
     </main>
   )
 }
