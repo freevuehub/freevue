aa7dc814b0319e9fe48b783da31b240087cca9f7
freevuehub <hackish113@gmail.com>
2023-11-16 12:50:32 +0900

    포스트 리스트 틀 작업

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,19 @@
 'use server'
 
-import Link from 'next/link'
-import { pipe, map, toArray, toAsync } from '@fxts/core'
-import { Card } from '~/components'
+import { Card, PostList } from '~/components'
 import { getPostList } from '~/API'
 
 import type { NextPage } from 'next'
 
 const Home: NextPage = async () => {
-  const data = await pipe(
-    '',
-    getPostList,
-    toAsync,
-    map(({ id, properties }) => ({
-      id,
-      title: 'title' in properties.title ? properties.title.title[0].plain_text : '',
-    })),
-    toArray
-  )
+  const data = await getPostList()
 
   return (
     <>
       <Card className="my-[120px]">
         <p className="dark:text-white">배포 실수로 블로그 날라감..</p>
       </Card>
-      {pipe(
-        data,
-        map((item) => (
-          <Link key={item.id} href={`/${item.id}`}>
-            <Card>
-              <p className="dark:text-white">{item.title}</p>
-            </Card>
-          </Link>
-        )),
-        toArray
-      )}
+      <PostList list={data} />
     </>
   )
 }
