12464af563a7103ae437ca966d82a478213dbdc6
freevuehub <hackish113@gmail.com>
2023-11-16 12:50:03 +0900

    노션 타입 정리

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,21 @@
+import type { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints'
+
+export type * from '@notionhq/client/build/src/api-endpoints'
+
 export type IClassName = string
+
 export type IChild = {
   children?: React.ReactNode
   className?: IClassName
+}
+
+export type PostProperties = {
+  id: string
+  title: PageObjectResponse['properties']['title']
+  thumbnail: PageObjectResponse['properties']['thumbnail']
+  summary: PageObjectResponse['properties']['summary']
+  tags: PageObjectResponse['properties']['tags']
+  category: PageObjectResponse['properties']['category']
+  date: PageObjectResponse['properties']['date']
+  properties: PageObjectResponse['properties']
 }
