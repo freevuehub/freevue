6c7639c456a8e4ec31890ad86e532e0f1ad9cddd
freevuehub <hackish113@gmail.com>
2024-08-10 19:32:26 +0900

    .

---
+++ apps/www/types/index.d.ts
@@ -0,0 +1,26 @@
+import type { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints'
+
+export type * from '@notionhq/client/build/src/api-endpoints'
+
+export type IClassName = string
+
+export type IChild = {
+  children?: React.ReactNode
+  className?: IClassName
+}
+
+export type NextProps = {
+  params: Record<string, string>
+  searchParams: Record<string, string>
+}
+
+export type PostProperties = {
+  id: string
+  title: PageObjectResponse['properties']['title']
+  thumbnail: PageObjectResponse['properties']['thumbnail']
+  summary: PageObjectResponse['properties']['summary']
+  tags: PageObjectResponse['properties']['tags']
+  category: PageObjectResponse['properties']['category']
+  date: PageObjectResponse['properties']['date']
+  properties: PageObjectResponse['properties']
+}
