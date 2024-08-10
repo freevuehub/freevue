ebf107b15c05534d3f3a334c8b1237daf57f7a2c
freevuehub <hackish113@gamil.com>
2024-08-10 19:17:58 +0900

    .

---
+++ apps/www/API/index.ts
@@ -0,0 +1,176 @@
+import { Client } from '@notionhq/client'
+import { NotionToMarkdown } from 'notion-to-md'
+import { pipe, prop, toAsync, head, toArray, map } from '@fxts/core'
+import { property } from '@notion/util'
+import { DB_ID, INITIAL_NOTION_PROPERTIES } from '~/constant'
+
+import type { PageObjectResponse, PostProperties } from '~/types'
+
+const notion = new Client({
+  auth: process.env.NOTION_SECRET_KEY,
+})
+const notionToMarkdown = new NotionToMarkdown({ notionClient: notion })
+
+const pageToMarkdown = async (id: string) => {
+  try {
+    return (
+      (await pipe(
+        notionToMarkdown.pageToMarkdown(id),
+        toAsync,
+        toArray,
+        (data) => notionToMarkdown.toMarkdownString(data),
+        prop('parent')
+      )) || ''
+    )
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
+
+export const getPostList = async (equals?: string) => {
+  try {
+    return (await pipe(
+      notion.databases.query({
+        database_id: DB_ID,
+        // page_size: 10,
+        filter: {
+          and: [
+            {
+              property: 'type',
+              type: 'select',
+              select: {
+                equals: 'Post',
+              },
+            },
+            {
+              property: 'status',
+              type: 'select',
+              select: {
+                equals: 'Public',
+              },
+            },
+            {
+              property: 'category',
+              type: 'select',
+              select: {
+                equals: equals || '',
+              },
+            },
+          ],
+        },
+      }),
+      prop('results'),
+      map((item) => ({
+        id: item.id,
+        properties: 'properties' in item ? item.properties : {},
+      })),
+      map((item) => ({
+        ...item,
+        title: item.properties.title,
+        thumbnail: item.properties.thumbnail,
+        summary: item.properties.summary,
+        tags: item.properties.tags,
+        category: item.properties.category,
+        date: item.properties.date,
+      })),
+      toArray
+    )) as PostProperties[]
+  } catch {
+    return []
+  }
+}
+
+export const getPostBySlug = async (id: string) => {
+  return (await pipe(
+    [
+      notion.databases.query({
+        database_id: 'aaffdb0decad4da5ac6c15456dfe22f7',
+        filter: {
+          or: [
+            {
+              property: 'slug',
+              type: 'rich_text',
+              rich_text: {
+                equals: id,
+              },
+            },
+          ],
+        },
+      }),
+    ],
+    toAsync,
+    head,
+    prop('results')
+  )) as PageObjectResponse[]
+}
+
+export const getPageBlocks = async (id: string): Promise<string> => {
+  try {
+    return await pipe(id, pageToMarkdown)
+  } catch {
+    const [post] = await getPostBySlug(id)
+
+    if (!!post) {
+      return await getPageBlocks(post.id)
+    }
+
+    return ''
+  }
+}
+
+export const getPageProperties = async (id: string): Promise<PageObjectResponse> => {
+  try {
+    const data = await pipe(id, (id) =>
+      notion.pages.retrieve({
+        page_id: id,
+      })
+    )
+
+    if ('properties' in data) {
+      return data
+    }
+
+    return INITIAL_NOTION_PROPERTIES
+  } catch {
+    const [post] = await getPostBySlug(id)
+
+    if (!!post) {
+      return await getPageProperties(post.id)
+    }
+
+    return INITIAL_NOTION_PROPERTIES
+  }
+}
+
+export const getPagePropertyById =
+  (propertyId: string) =>
+  async (id: string): Promise<string> => {
+    try {
+      const data = await pipe(
+        {
+          page_id: id,
+          property_id: propertyId,
+        },
+        notion.pages.properties.retrieve
+      )
+
+      if ('files' in data) {
+        return pipe(data.files[0], property('name')) || ''
+      }
+      if ('results' in data) {
+        return pipe(data.results[0], property('title'), prop('plain_text')) || ''
+      }
+
+      return ''
+    } catch {
+      const [post] = await getPostBySlug(id)
+
+      if (!!post) {
+        return await pipe(post.id, getPagePropertyById(propertyId))
+      }
+
+      return ''
+    }
+  }
+
+export default notion
---
+++ apps/www/components/Markdown/index.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import { Prism } from 'react-syntax-highlighter'
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
+import type { IChild } from '~/types'
+
+type Props = {} & IChild
+
+const MarkdownRender: React.FC<Props> = (props) => {
+  return (
+    <div className="flex flex-col gap-[10px] mt-10">
+      <Markdown
+        rehypePlugins={[remarkGfm]}
+        components={{
+          code({ className, children }) {
+            const match = /language-(\w+)/.exec(className || '')
+
+            return match ? (
+              <Prism language={match[1]} style={dracula} className="scroll-hidden">
+                {String(children).replace(/\n$/, '')}
+              </Prism>
+            ) : (
+              <code className="bg-content px-[8px] py-[2px] rounded-[5px] text-primary">
+                {children}
+              </code>
+            )
+          },
+          p({ children }) {
+            return <p className="dark:text-white text-[16px] leading-[24px]">{children}</p>
+          },
+          blockquote({ children }) {
+            return (
+              <div className="border-[1px] border-primary border-solid p-[10px] my-[10px] dark:bg-[#282828]">
+                {children}
+              </div>
+            )
+          },
+          h1({ children }) {
+            return <h1 className="dark:text-white text-5xl mt-6 font-taebaek">{children}</h1>
+          },
+          h2({ children }) {
+            return <h2 className="dark:text-white text-4xl mt-6 font-taebaek">{children}</h2>
+          },
+          h3({ children }) {
+            return <h3 className="dark:text-white text-2xl mt-6 font-taebaek">{children}</h3>
+          },
+          ul({ children }) {
+            return (
+              <ol className="dark:text-white pl-7 flex flex-col gap-1 list-disc">{children}</ol>
+            )
+          },
+          ol({ children }) {
+            return (
+              <ol className="dark:text-white pl-7 flex flex-col gap-1 list-decimal">{children}</ol>
+            )
+          },
+          li({ children }) {
+            return <li className="dark:text-white text-[16px] leading-[24px]">{children}</li>
+          },
+        }}
+      >
+        {String(props.children)}
+      </Markdown>
+    </div>
+  )
+}
+
+export default MarkdownRender
